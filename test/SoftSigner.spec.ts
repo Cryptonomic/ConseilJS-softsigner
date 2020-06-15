aa8253a097caac58ff7655adfd2562dfaa337c65
anonymoussprocket <anonymoussprocket@gmail.com>
2020-06-15 19:26:20 -0400

    - updated tests

--- test/SoftSigner.spec.ts
+++ test/SoftSigner.spec.ts
@@ -18,10 +18,11 @@ describe('SoftSigner tests', () => {
     });
 
     it('sign', async () => {
-        const result = await signer.signOperation(Buffer.from('Tacos Burritos', 'utf8'))
+        const forgedOperationGroup = 'f58c43b69cf5fa7e6183ea3d899a480c9b5b3d6dadea68ee2e5c21ae1cc767ab6c0034a00f9b7964943b4ab583a8d1f7241a0cb9742cd20be807904eac02e80700002c0b1b21166a60a985fd8f11b567c445382fbd8300';
+        const result = await signer.signOperation(Buffer.from('03' + forgedOperationGroup, 'hex'));
         const signature = TezosMessageUtils.readSignatureWithHint(result, 'edsig');
 
-        expect(signature).to.equal('edsigtbmrgC8V2xU3Dc3n99v8CZk3cQAX1PcwbGRDkVkFSqax996qTPXsLryas9WBN9mCXiJFQSUiVkkkot6jQ4eEsU8rAt6jzW');
+        expect(signature).to.equal('edsigtyUK6MFziVBFXmEEnc1TuFsmcCCrke5nVG2uWLh4y1ydPFweD1C7Q4MDUqHQXkwEVwDmBMfp2ufc3MRi9MRM7kETd4vgnZ');
     });
 
     it('signText', async () => {
