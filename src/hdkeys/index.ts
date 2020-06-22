900e2201b9c44d818d1f9aeec8166face39b4526
itamar <itamar.reif@gmail.com>
2020-06-22 23:19:43 +0300

    more cleanup

--- src/hdkeys/index.ts
+++ src/hdkeys/index.ts
@@ -5,7 +5,9 @@ import * as params from './testvector-1.json';
 const HARDENED = 0x80000000;
 
 function BIP32Path(value: string): boolean {
-  return value.match(/^(m\/)?(\d+'?\/)*\d+'?$/) !== null;
+  return (
+      /*typeforce.String(value) && */
+      value.match(/^(m\/)?(\d+'?\/)*\d+'?$/) !== null);
 }
 
 // https://github.com/satoshilabs/slips/blob/master/slip-0010.md
@@ -19,6 +21,16 @@ class HDNode {
         this.privateKey = privateKey;
         this.chainCode = chainCode;
         this.publicKey = publicKey;
+    }
+
+    static fromSeed(seed: Buffer, curve: crypto.CurveInfo): HDNode {
+        // need to verify seed length is 512 bits
+        let i = crypto.hmacSHA512(Buffer.from(curve.bip32Name, 'utf8'), seed);
+        const iL = i.slice(0, 32);
+        const iR = i.slice(32);
+        let ret = new HDNode(iL, iR, undefined);
+        ret.depth = 0;
+        return ret;
     }
 
     isNeutered(): boolean {
@@ -50,19 +62,46 @@ class HDNode {
         ret.depth = this.depth + 1;
         return ret;
     }
+
+    // derivePath(path: string): HDNode {
+    //     // typeforce(BIP32Path, path);
+
+    //     let splitPath = path.split('/');
+    //     if (splitPath[0] === 'm') {
+    //         if (this.parentFingerprint)
+    //         throw new TypeError('Expected master, got child');
+
+    //         splitPath = splitPath.slice(1);
+    //     }
+
+    //     return splitPath.reduce(
+    //         (prevHd, indexStr) => {
+    //         let index;
+    //         if (indexStr.slice(-1) === `'`) {
+    //             index = parseInt(indexStr.slice(0, -1), 10);
+    //             return prevHd.deriveHardened(index);
+    //         } else {
+    //             index = parseInt(indexStr, 10);
+    //             return prevHd.derive(index);
+    //         }
+    //         },
+    //         this as HDNode,
+    //     );
+    // }
 }
 
-function fromSeed(seed: Buffer, curve: crypto.CurveInfo): HDNode {
-        // need to verify seed length is 512 bits
-        let i = crypto.hmacSHA512(Buffer.from(curve.bip32Name, 'utf8'), seed);
-        const iL = i.slice(0, 32);
-        const iR = i.slice(32);
-        let ret = new HDNode(iL, iR, undefined);
-        ret.depth = 0;
-        return ret;
-    }
+async function main() {
+    // // get pubkey from ledger
+    // let ledger = await ledgersigner.TezosLedgerConnector.getInstance();
+    // console.log(await ledger.getAddress(derivationPath, true, ledgersigner.Curve.SECP256K1));
+    // console.log("ledger\n" + (await ledgerKeyStore.getTezosPublicKey(derivationPath)));
 
-function main() {
+    // just stored this in a const above
+    // let seed = (await bip39.mnemonicToSeed(mnemonic, passphrase)).toString("hex");
+    // console.log(seed);
+
+    // let bip32Node = bip32.fromSeed(Buffer.from(seed, 'utf8'));
+
     let node: HDNode = HDNode.fromSeed(Buffer.from(params.seed, 'hex'), crypto.SECP256K1);
     console.log(node.privateKey.toString('hex'));
     let child0 = node.derive(0 + HARDENED);
