7148996b79d137f556bd76ea2b7e1822417f71da
anonymoussprocket <anonymoussprocket@gmail.com>
2020-12-07 20:26:32 -0500

    - key password is now an optional parameter - test update - npm outdated - bumped version

--- src/SoftSigner.ts
+++ src/SoftSigner.ts
@@ -1,18 +1,14 @@
-import * as GeneratePassword from 'generate-password'
 import { Signer, SignerCurve, TezosMessageUtils } from 'conseiljs';
 
 import { CryptoUtils } from './utils/CryptoUtils'
 
 /**
- * libsodium/ed25519
+ * Softsigner is a wrapper for libsodium. It leverages the ED25519 curve to perform cryptographic operations relevant for the Tezos blockchain.
  */
 export class SoftSigner implements Signer {
     readonly _secretKey: Buffer;
-    private _passphrase: string;
+    private _isEncrypted: boolean;
     private _salt: Buffer;
-    private _key: Buffer;
-    private _lockTimout: number
-    private _unlocked: boolean;
 
     /**
      * 
@@ -21,64 +17,42 @@ export class SoftSigner implements Signer {
      * @param validity Duration for keeping the key decrypted in memory.
      * @param passphrase
      */
-    private constructor(secretKey: Buffer, validity: number = -1, passphrase: string = '', salt?: Buffer) {
+    private constructor(secretKey: Buffer, isEncrypted: boolean = false, salt?: Buffer) {
         this._secretKey = secretKey;
-        this._lockTimout = validity;
-        this._passphrase = passphrase;
+        this._isEncrypted = isEncrypted;
         this._salt = salt ? salt : Buffer.alloc(0);
-
-        this._unlocked = validity < 0;
-        this._key = Buffer.alloc(0);
-
-        if (validity < 0) {
-            this._key = secretKey;
-        }
     }
 
     public getSignerCurve(): SignerCurve {
         return SignerCurve.ED25519
     }
 
-    public static async createSigner(secretKey: Buffer, validity: number = 60): Promise<Signer> {
-        if (validity >= 0) {
-            const passphrase = GeneratePassword.generate({ length: 32, numbers: true, symbols: true, lowercase: true, uppercase: true });
+    public static async createSigner(secretKey: Buffer, password: string = ''): Promise<Signer> {
+        if (password.length > 0) {
             const salt = await CryptoUtils.generateSaltForPwHash();
-            secretKey = await CryptoUtils.encryptMessage(secretKey, passphrase, salt);
-            return new SoftSigner(secretKey, validity, passphrase, salt);
-        } else {
-            return new SoftSigner(secretKey);
+            const encryptedKey = await CryptoUtils.encryptMessage(secretKey, password, salt);
+            return new SoftSigner(encryptedKey, true, salt);
         }
+
+        return new SoftSigner(secretKey);
     }
 
-    private async getKey() {
-        if (!this._unlocked) {
-            const k = await CryptoUtils.decryptMessage(this._secretKey, this._passphrase, this._salt);
-            if (this._lockTimout == 0) {
-                return k;
-            }
-
-            this._key = k;
-            this._unlocked = true;
-            if (this._lockTimout > 0) {
-                setTimeout(() => {
-                    this._key = Buffer.alloc(0);
-                    this._unlocked = false;
-                }, this._lockTimout * 1000);
-            }
-            return this._key;
+    private async getKey(password: string = '') {
+        if (this._isEncrypted && password.length > 0) {
+            return await CryptoUtils.decryptMessage(this._secretKey, password, this._salt);
         }
 
-        return this._key;
+        return this._secretKey;
     }
 
     /**
-     * Signs a 
+     * This method in intended to sign Tezos operations. It produces a 32-byte blake2s hash prior to signing the buffer.
      * 
      * @param {Buffer} bytes Bytes to sign
      * @returns {Buffer} Signature
      */
-    public async signOperation(bytes: Buffer): Promise<Buffer> {
-        return CryptoUtils.signDetached(TezosMessageUtils.simpleHash(bytes, 32), await this.getKey());
+    public async signOperation(bytes: Buffer, password: string = ''): Promise<Buffer> {
+        return CryptoUtils.signDetached(TezosMessageUtils.simpleHash(bytes, 32), await this.getKey(password));
     }
 
     /**
@@ -87,8 +61,8 @@ export class SoftSigner implements Signer {
      * @param message UTF-8 text
      * @returns {Promise<string>} base58check-encoded signature prefixed with 'edsig'
      */
-    public async signText(message: string): Promise<string> {
-        const messageSig = await CryptoUtils.signDetached(Buffer.from(message, 'utf8'), await this.getKey());
+    public async signText(message: string, password: string = ''): Promise<string> {
+        const messageSig = await CryptoUtils.signDetached(Buffer.from(message, 'utf8'), await this.getKey(password));
 
         return TezosMessageUtils.readSignatureWithHint(messageSig, 'edsig');
     }
@@ -99,9 +73,8 @@ export class SoftSigner implements Signer {
      * @param message UTF-8 text
      * @returns {Promise<string>} base58check-encoded signature prefixed with 'edsig'
      */
-    public async signTextHash(message: string): Promise<string> {
-        const messageHash = TezosMessageUtils.simpleHash(Buffer.from(message, 'utf8'), 32);
-        const messageSig = await CryptoUtils.signDetached(messageHash, await this.getKey());
+    public async signTextHash(message: string, password: string = ''): Promise<string> {
+        const messageSig = await this.signOperation(Buffer.from(message, 'utf8'), password);
 
         return TezosMessageUtils.readSignatureWithHint(messageSig, 'edsig');
     }
